4b59baea29e6a55de67fd41a3aa04fea7cecb37e
sohelallauddin.d <devlekarsohel333@gmail.com>
2023-06-21 12:46:51 +0530

    singly link list deletion of node at Start and at the end.

--- singleLinkList.cpp
+++ singleLinkList.cpp
@@ -10,7 +10,7 @@ class Node
 
 Node *START = NULL;
 
-Node *createNode() // crating node function on heap
+Node *createNode() // creating on heap
 {
    Node *n = new Node();
    return n;
@@ -19,18 +19,19 @@ Node *createNode() // crating node function on heap
 void insertNode()
 {
 
-  Node *temp, *t ;
+  Node *temp, *t ; 
   
   temp = createNode(); // creating node 
   cout<<" please entre the value for list ";
+
   cin>>temp->data;
   temp->next = NULL; 
+
   if(START==NULL) // if list is empty 
   {
-     START = temp; // creting node for start or adding node at begining.
-  } 
-  // when list is not empty create node pointer for traversing the list and attached node at the end of list. 
-  else 
+     START = temp; // adding node at begining.
+  }  
+  else // adding the node at the end
   {  
      t = START; 
      while(t->next != NULL)
@@ -42,31 +43,91 @@ void insertNode()
 
 }
 
-void display( Node *dis)
+void atStartDeletionNode() //deletion of node at the start
 {
-   if(dis==NULL)
+   Node *t;
+   if (START == NULL)
+   {
+      cout<<" The list is empty"<<"\n";
+   }
+   else 
+   {
+      t=START;
+      START=START->next;
+      delete t;
+   }
+   
+
+}
+
+void atEndDeletionNode()  //  deletion of node at the end
+{
+  
+   Node *t;
+   Node *t1;
+   if ( START == NULL)
+   {
+      cout<<" the list is empty "<<"\n";
+   }
+
+   else if(START->next == NULL) // only node of the list deleted
+   {
+         START = NULL;
+         delete START;
+         cout<<"\n only list of the node deleted";
+   }
+
+   else // last node of node deleteded
+   {
+     t = START;
+     while(t->next!=NULL)
+     {
+       t1 = t;
+       t  = t->next;
+     }
+     t1->next = NULL;
+     delete t; 
+   }
+
+}
+
+void display( )
+{
+   Node *t;
+   if(START==NULL)
    {
      cout<< "list is empty"<<"\n";
    }
    else
    {
-      while(dis->next!= NULL)
+      t=START;
+      while(t!= NULL)
       {  
-        cout<< " the list data " << dis->data<<"\n";
-        dis=dis->next;
+        cout<< " the list data " << t->data<<"\n";
+        t=t->next;
       }
-      cout<< " the list data " << dis->data<<"\n";
-   }
-
+   }   
 }
+
 int main()
 {
    insertNode();
    insertNode();
    insertNode();
    insertNode();
+   insertNode();
 
-   display(START);
-
+   display();
+   // deletion of node at the start
+   atStartDeletionNode();
+   cout<<" =================================== "<<"\n";
+   cout<<" the list is after deletion at start :"<<"\n";
+   display();
+   
+   // deletion of node at the end.
+   atEndDeletionNode();
+   cout<<" =================================== "<<"\n";
+   cout<<" the list is after deletion at end : "<<"\n";
+   display();
 }
 
